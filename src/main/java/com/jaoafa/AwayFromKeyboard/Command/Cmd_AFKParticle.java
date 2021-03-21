fcb56af464487fcd1768a9b04b54fd7bd708132c
Tomachi [ICHIGO] <lego@aol.jp>
2021-03-22 02:50:28 +0900

    change: DRIP_WATERとDRIP_LAVAを更新

--- src/main/java/com/jaoafa/AwayFromKeyboard/Command/Cmd_AFKParticle.java
+++ src/main/java/com/jaoafa/AwayFromKeyboard/Command/Cmd_AFKParticle.java
@@ -1,9 +1,9 @@
 package com.jaoafa.AwayFromKeyboard.Command;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.jaoafa.AwayFromKeyboard.Library.AFKParticle;
+import com.jaoafa.AwayFromKeyboard.Library.Library;
+import com.jaoafa.AwayFromKeyboard.Library.MonoPlayerVote;
+import com.jaoafa.AwayFromKeyboard.Main;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,66 +11,66 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import com.jaoafa.AwayFromKeyboard.Main;
-import com.jaoafa.AwayFromKeyboard.Library.AFKParticle;
-import com.jaoafa.AwayFromKeyboard.Library.Library;
-import com.jaoafa.AwayFromKeyboard.Library.MonoPlayerVote;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cmd_AFKParticle implements CommandExecutor, TabCompleter {
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "このコマンドはプレイヤーのみ使用できます。");
-			return true;
-		}
-		Player player = (Player) sender;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "このコマンドはプレイヤーのみ使用できます。");
+            return true;
+        }
+        Player player = (Player) sender;
 
-		MonoPlayerVote mpv = new MonoPlayerVote(player);
-		AFKParticle[] availableParts = Library.getAvailableAFKParticle(mpv.getVoteCount());
+        MonoPlayerVote mpv = new MonoPlayerVote(player);
+        AFKParticle[] availableParts = Library.getAvailableAFKParticle(mpv.getVoteCount());
 
-		if (args.length == 0) {
-			sender.sendMessage(
-					"[AFKParticle] " + ChatColor.GREEN + "次のパーティクルが設定できます: "
-							+ Arrays.stream(availableParts).map(Enum::name)
-									.collect(Collectors.joining(", ")));
-			return true;
-		}
+        if (args.length == 0) {
+            sender.sendMessage(
+                "[AFKParticle] " + ChatColor.GREEN + "次のパーティクルが設定できます: "
+                    + Arrays.stream(availableParts).map(Enum::name)
+                    .collect(Collectors.joining(", ")));
+            return true;
+        }
 
-		if (args[0].equalsIgnoreCase("null")) {
-			mpv.setSelectedParticle(null);
-			sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "AFKパーティクルをリセットしました。最後に解放されたものが自動的に選択されます。");
-			return true;
-		}
+        if (args[0].equalsIgnoreCase("null")) {
+            mpv.setSelectedParticle(null);
+            sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "AFKパーティクルをリセットしました。最後に解放されたものが自動的に選択されます。");
+            return true;
+        }
 
-		AFKParticle particle = AFKParticle.fromString(args[0]);
-		if (particle == null) {
-			sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "指定されたパーティクルは存在しないか利用できません。");
-			return true;
-		}
-		if (!Arrays.asList(availableParts).contains(particle)) {
-			sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "指定されたパーティクルはあなたの投票数では利用できません。");
-			return true;
-		}
+        AFKParticle particle = AFKParticle.fromString(args[0]);
+        if (particle == null) {
+            sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "指定されたパーティクルは存在しないか利用できません。");
+            return true;
+        }
+        if (!Arrays.asList(availableParts).contains(particle)) {
+            sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "指定されたパーティクルはあなたの投票数では利用できません。");
+            return true;
+        }
 
-		mpv.setSelectedParticle(particle);
+        mpv.setSelectedParticle(particle);
 
-		sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "AFKパーティクルを設定しました。");
-		return true;
-	}
+        sender.sendMessage("[AFKParticle] " + ChatColor.GREEN + "AFKパーティクルを設定しました。");
+        return true;
+    }
 
-	@Override
-	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
-		if (!(sender instanceof Player)) {
-			return Main.getJavaPlugin().onTabComplete(sender, cmd, alias, args);
-		}
-		Player player = (Player) sender;
-		MonoPlayerVote mpv = new MonoPlayerVote(player);
-		AFKParticle[] availableParts = Library.getAvailableAFKParticle(mpv.getVoteCount());
-		if (args.length == 1) {
-			return Arrays.stream(availableParts)
-					.filter(part -> part.name().toUpperCase().startsWith(args[0].toUpperCase()))
-					.map(Enum::name).collect(Collectors.toList());
-		}
-		return Arrays.stream(availableParts).map(Enum::name).collect(Collectors.toList());
-	}
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+        if (!(sender instanceof Player)) {
+            return Main.getJavaPlugin().onTabComplete(sender, cmd, alias, args);
+        }
+        Player player = (Player) sender;
+        MonoPlayerVote mpv = new MonoPlayerVote(player);
+        AFKParticle[] availableParts = Library.getAvailableAFKParticle(mpv.getVoteCount());
+        if (args.length == 1) {
+            return Arrays.stream(availableParts)
+                .map(Enum::name)
+                .filter(name -> name.toUpperCase().startsWith(args[0].toUpperCase()))
+                .collect(Collectors.toList());
+        }
+        return Arrays.stream(availableParts).map(Enum::name).collect(Collectors.toList());
+    }
 }
