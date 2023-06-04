e9b74f67d8d547bb40520ce470b4a9efe0ab1517
Tomachi <8929706+book000@users.noreply.github.com>
2023-06-04 16:27:12 +0900

    feat: JDA v5.0.0-beta.9に対応 (#62)

--- src/main/java/com/jaoafa/AwayFromKeyboard/Event/Event_DiscordReady.java
+++ src/main/java/com/jaoafa/AwayFromKeyboard/Event/Event_DiscordReady.java
@@ -1,14 +1,14 @@
 package com.jaoafa.AwayFromKeyboard.Event;
 
 import com.jaoafa.AwayFromKeyboard.Main;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.hooks.SubscribeEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.logging.Logger;
 
-public class Event_DiscordReady {
-    @SubscribeEvent
-    public void onReadyEvent(ReadyEvent event) {
+public class Event_DiscordReady extends ListenerAdapter {
+    @Override
+    public void onReady(ReadyEvent event) {
         Main.ServerChatChannel = event.getJDA().getTextChannelById(Main.ServerChatID);
         Logger logger = Main.getMain().getLogger();
         if (Main.ServerChatChannel != null) {
